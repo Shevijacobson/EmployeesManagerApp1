7d54b3c6460d3aa5100ab37da5e38fa5e19b418a
shevijacobson <shevijacob@gmail.com>
2024-07-25 23:52:53 +0300

    Display candidates and set the options of filterBy

--- AddEmployee.xaml.cs
+++ AddEmployee.xaml.cs
@@ -26,11 +26,12 @@ namespace UI
         public event IsAddEmployee OnAddEmployee;
         public EmployeeManagerBLL employeeManagerBll { get; set; }
         public List<string> AllRolesInCompany { get; set; }
-        public IsAddEmployee isAddEmployeeDelegate;
+        //public IsAddEmployee isAddEmployeeDelegate;
+
         public AddEmployee()
         {
             employeeManagerBll = new EmployeeManagerBLL();
-            OnAddEmployee += check;
+
             InitializeComponent();
             AllRolesInCompany = employeeManagerBll.GetAllRoleInCompany();
 
@@ -38,86 +39,51 @@ namespace UI
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            
-            Regex emailRegex = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}");
-            Regex idRegex = new Regex("^[0-9]{9}$");
-            Regex nameRegex = new Regex(@"^[a-zA-Z]{2,}$");
-            Regex ageRegex = new Regex(@"^([2-5][0-9]|6[0-7]|1[8-9])$");
-            Regex phoneRegex = new Regex("^[0-9]{10}$");
-       
-            List<string> invalidFields = new List<string>();
+            Employee employee = new Employee();
+            int idText, age, year;
 
-            if (!idRegex.IsMatch(IdTextBox.Text))
+            bool idIsNumber = int.TryParse(IdTextBox.Text, out idText);
+            bool ageIsNumber = int.TryParse(AgeTextBox.Text, out age);
+            bool yearIsNumber = int.TryParse(StartOfWorkingYearTextBox.Text, out year);
+            if (idIsNumber && ageIsNumber && yearIsNumber)
             {
-                invalidFields.Add("ID");
-            }
-
-            if (!nameRegex.IsMatch(FirstNameTextBox.Text))
-            {
-                invalidFields.Add("First Name");
-            }
-            if(!nameRegex.IsMatch(LastNameTextBox.Text))
-            {
-                invalidFields.Add("Last Name");
-            }
-            if (!ageRegex.IsMatch(AgeTextBox.Text))
-            {
-                invalidFields.Add("Age");
-            }
-
-            if (!phoneRegex.IsMatch(PhoneNumberTextBox.Text))
-            {
-                invalidFields.Add("Phone Number");
-            }
-
-            if (!emailRegex.IsMatch(MailAddressTextBox.Text))
-            {
-                invalidFields.Add("Email Address");
-            }
-
-            if (!AllRolesInCompany.Contains(JobTitleTextBox.Text))
-            {
-                invalidFields.Add("Job Title");
-            }
-
-            if (invalidFields.Count > 0)
-            {
-                string errorMessage = "The following fields have invalid data: " + string.Join(", ", invalidFields);
-                MessageBox.Show(errorMessage);
-            }
-            else
-            {
-              
-                //פונקציה שמוסיפה עובד למערך העובדים בדטה
-                Employee employee = new Employee();
-                employee.Id = int.Parse(IdTextBox.Text) ;
-                employee.FirstName=FirstNameTextBox.Text ;
-                employee.LastName=LastNameTextBox.Text ;
-                employee.PhoneNumber=PhoneNumberTextBox.Text ;
+                employee.Id = idText;
                 employee.Age = int.Parse(AgeTextBox.Text);
                 employee.StartOfWorkYear = int.Parse(StartOfWorkingYearTextBox.Text);
+
+                employee.FirstName = FirstNameTextBox.Text;
+                employee.LastName = LastNameTextBox.Text;
+                employee.PhoneNumber = PhoneNumberTextBox.Text;
                 employee.City = CityAddressTextBox.Text;
                 employee.Street = StreetAddressTextBox.Text;
                 employee.RoleInCompany = JobTitleTextBox.Text;
                 employee.PhoneNumber = PhoneNumberTextBox.Text;
                 employee.Email = MailAddressTextBox.Text;
 
-                OnAddEmployee(employee);
 
-                MessageBox.Show("Employee added Successfully");
+                if (OnAddEmployee(employee) == true)
+                {
+                    IdTextBox.Text = "";
+                    FirstNameTextBox.Text = "";
+                    LastNameTextBox.Text = "";
+                    PhoneNumberTextBox.Text = "";
+                    AgeTextBox.Text = "";
+                    StartOfWorkingYearTextBox.Text = "";
+                    CityAddressTextBox.Text = "";
+                    StreetAddressTextBox.Text = "";
+                    JobTitleTextBox.Text = "";
+                    PhoneNumberTextBox.Text = "";
+                    MailAddressTextBox.Text = "";
+                }
             }
-            this.Close();
+    
         }
 
-        public bool check(Employee employee)
-        {
-            return true;
-        }
 
         private void FinishButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-        }
 
+        }
     }
 }
--- FindCandidate.xaml.cs
+++ FindCandidate.xaml.cs
@@ -3,6 +3,7 @@ using DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace UI
 {
     /// <summary>
@@ -22,13 +24,75 @@ namespace UI
     public partial class FindCandidate : Window
     {
         public EmployeeManagerBLL EmployeeManagerBLL { get; set; }
-        public List<Candidate>  ListOfCandidate { get; set; }
+        public List<Candidate> ListOfCandidate { get; set; }
+        public Dictionary<int, List<InformationInterviewer>> DicIdCandidateToAllIntreview { get; set; }
+        public List<Interview> ListOfInterview { get; set; }
+        public List<Employee> ListOfEmployee { get; set; }
+
         public FindCandidate()
         {
-            DataContext=this;
+
+
             EmployeeManagerBLL = new EmployeeManagerBLL();
-            ListOfCandidate= EmployeeManagerBLL.GetAllCandidates();  
             InitializeComponent();
+
+            DataContext = this;
+            ListOfCandidate = EmployeeManagerBLL.GetAllCandidates();
+            ListOfInterview = EmployeeManagerBLL.GetAllInterview();
+            ListOfEmployee = EmployeeManagerBLL.GetAllEmployees();
+            DicIdCandidateToAllIntreview = new Dictionary<int, List<InformationInterviewer>>();
+
+            BuildDicIdCandidateToIntreview();
+
         }
+        public void BuildDicIdCandidateToIntreview()
+        {
+
+            var lstInterviews = new List<InformationInterviewer>();
+            Dictionary<int, Employee> DicEmployeeById = new Dictionary<int, Employee>();
+
+            foreach (var employee in ListOfEmployee)
+            {
+                DicEmployeeById.Add(employee.Id, employee);
+            }
+
+            foreach (Candidate candidate in ListOfCandidate)
+            {
+
+                lstInterviews = ListOfInterview.Where(interview => interview.CandidateId == candidate.Id)
+                .OrderBy(iterview => iterview.InterviewDate)
+
+               .Select(interview => new InformationInterviewer
+               {
+                  
+                   InterviewNumber = interview.InterviewNumber,
+                   InterviewDate = interview.InterviewDate,
+                   RoleInCompany = interview.RoleInCompany,
+                   NameInterviewer = DicEmployeeById.ContainsKey(interview.InterviewerId) ?
+                   DicEmployeeById[interview.InterviewerId].FirstName + " " + DicEmployeeById[interview.InterviewerId].LastName :
+                  "Interviewer Not Found",
+
+                   PhoneInterviewer = DicEmployeeById.ContainsKey(interview.InterviewerId) ?
+                   DicEmployeeById[interview.InterviewerId].PhoneNumber : "Phone Not Found",
+               }).ToList();
+
+                DicIdCandidateToAllIntreview.Add(candidate.Id, lstInterviews);
+
+            }
+
+        }
+
+        public void FilterByselectedInterviewer(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = (sender as ComboBox).SelectedItem;
+
+            if (selectedItem != null && DicIdCandidateToAllIntreview.ContainsKey(((Candidate)selectedItem).Id))
+            {
+                DataGridInterviews.ItemsSource = DicIdCandidateToAllIntreview[((Candidate)selectedItem).Id];
+            }
+
+        }
+
+
     }
 }
